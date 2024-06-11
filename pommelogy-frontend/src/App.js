09e17f777bb83c22f3b64aa9152ed2ba352d43f5
Jitali24 <jitalihhadiya@gmail.com>
2024-06-11 13:22:41 -0400

    [ADD] Addition of SignUp

--- pommelogy-frontend/src/App.js
+++ pommelogy-frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import SignUp from './components/Signup'
+// import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      {/*<Header />*/}
+      {/* <Home /> */}
+      <SignUp />
+      <Routes>
+        {/*<Route path='/' exact component={Home} />*/}
+        <Route path='/signup' exact component={SignUp} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
