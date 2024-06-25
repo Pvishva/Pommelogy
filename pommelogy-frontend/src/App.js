f399b487d2ef84138c5987d2974ba268d04213b5
Jitali Hadiya <70791610+Jitali24@users.noreply.github.com>
2024-06-25 12:24:51 -0400

    Update App.js

--- pommelogy-frontend/src/App.js
+++ pommelogy-frontend/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import SignUp from './components/Signup'
-// import Home from './components/Home'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Home from './components/Home'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Search from './components/Search'
+import Variety from './components/Variety'
 
 function App() {
   return (
     <Router>
-      {/*<Header />*/}
-      {/* <Home /> */}
-      <SignUp />
       <Routes>
-        {/*<Route path='/' exact component={Home} />*/}
-        <Route path='/signup' exact component={SignUp} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/variety' element={<Variety />} />
       </Routes>
     </Router>
   )
